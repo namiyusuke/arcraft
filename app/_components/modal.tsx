6d662099bf1040644e39bee36e3c1d8f2ce8ce1e
namikawayusuke <yu.namikawa18@gmail.com>
2025-08-07 01:32:50 +0900

    faf

--- app/_components/modal.tsx
+++ app/_components/modal.tsx
@@ -14,13 +14,20 @@ export default function Modal() {
           </Link>
           {
             <button
+              className="c-button"
               onClick={() => {
                 if (isScreenClicked) {
                   setIsScreenClicked(false);
                 }
               }}
             >
-              カメラを戻す
+              <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="25" cy="25" r="25" fill="#EFF1F3" />
+                <path
+                  d="M17.4 20.4L19.8 17H22.3L20.2 20H26.5C28.2239 20 29.8772 20.6848 31.0962 21.9038C32.3152 23.1228 33 24.7761 33 26.5C33 28.2239 32.3152 29.8772 31.0962 31.0962C29.8772 32.3152 28.2239 33 26.5 33H22L23 31H26.5C27.6935 31 28.8381 30.5259 29.682 29.682C30.5259 28.8381 31 27.6935 31 26.5C31 25.3065 30.5259 24.1619 29.682 23.318C28.8381 22.4741 27.6935 22 26.5 22H20.2L22.3 25H19.8L17.4 21.6L17 21L17.4 20.4Z"
+                  fill="#1E1E1E"
+                />
+              </svg>
             </button>
           }
         </div>
@@ -32,13 +39,20 @@ export default function Modal() {
           </Link>
           {
             <button
+              className="c-button"
               onClick={() => {
                 if (isDumbbleClicked) {
                   setIsDumbbleClicked(false);
                 }
               }}
             >
-              カメラを戻す
+              <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="25" cy="25" r="25" fill="#EFF1F3" />
+                <path
+                  d="M17.4 20.4L19.8 17H22.3L20.2 20H26.5C28.2239 20 29.8772 20.6848 31.0962 21.9038C32.3152 23.1228 33 24.7761 33 26.5C33 28.2239 32.3152 29.8772 31.0962 31.0962C29.8772 32.3152 28.2239 33 26.5 33H22L23 31H26.5C27.6935 31 28.8381 30.5259 29.682 29.682C30.5259 28.8381 31 27.6935 31 26.5C31 25.3065 30.5259 24.1619 29.682 23.318C28.8381 22.4741 27.6935 22 26.5 22H20.2L22.3 25H19.8L17.4 21.6L17 21L17.4 20.4Z"
+                  fill="#1E1E1E"
+                />
+              </svg>
             </button>
           }
         </div>
