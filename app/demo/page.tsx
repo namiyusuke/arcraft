4b80c9c4d1380b82e324e84106b842ae1eee9698
namikawayusuke <yu.namikawa18@gmail.com>
2025-08-29 22:31:27 +0900

    faf

--- app/demo/page.tsx
+++
@@ -1,9 +0,0 @@
-// import MomentumHoverDemo from "../_components/MomentumHoverDemo";
-
-// export default function DemoPage() {
-//   return (
-//     <main>
-//       <MomentumHoverDemo />
-//     </main>
-//   );
-// }
