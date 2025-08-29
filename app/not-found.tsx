a6f223ac61ce71d90089918d0bd85221eab27ea5
namikawayusuke <yu.namikawa18@gmail.com>
2025-08-29 22:41:01 +0900

    fafa

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 import styles from "./not-found.module.css";
-import Chatbot from "./_components/Chatbot";
-import KnowledgeSeeder from "./_components/KnowledgeSeeder";
+// import Chatbot from "./_components/Chatbot";
+// import KnowledgeSeeder from "./_components/KnowledgeSeeder";
 
 export default function NotFound() {
   return (
