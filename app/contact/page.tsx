361f04cfe71afb0bfcc6489e77a5c642f94d2ee5
namikawayusuke <yu.namikawa18@gmail.com>
2025-10-29 00:16:31 +0900

    fafa

---
+++ app/contact/page.tsx
@@ -0,0 +1,41 @@
+export default function ContactPage() {
+  return (
+    <>
+      <div className="">テスト</div>
+    </>
+  );
+}
+// "use client"
+
+// import { zodResolver } from "@hookform/resolvers/zod"
+// import { useForm } from "react-hook-form"
+// import * as z from "zod"
+
+// import { Button } from "@/components/ui/button"
+// import {
+//   Form,
+//   FormControl,
+//   FormDescription,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "@/components/ui/form"
+
+// // TODO(human): թ��n��������ޒ��WfO`UD
+
+// export default function ContactPage() {
+//   // TODO(human): useForm�ï�-�WfO`UD
+
+//   function onSubmit(values: z.infer<typeof formSchema>) {
+//     // թ����
+//     console.log(values)
+//   }
+
+//   return (
+//     <div className="container mx-auto max-w-2xl py-10">
+//       <h1 className="text-3xl font-bold mb-8">JOD�[</h1>
+//       {/* TODO(human): Form������Ȓ��WfO`UD */}
+//     </div>
+//   )
+// }
